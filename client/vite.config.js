863b340d84db33e3928b75f293f5f9a242efb112
techno-communications11 <ptharun@techno-communications.com>
2025-03-26 02:03:45 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'https://writeupapi.techno-communications.com/auth', // Backend server
+        target: 'https://writeupapi.techno-communications.com/auth',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     },
-  },
+    allowedHosts: ['writeup.techno-communications.com', 'localhost']
+  }
 });
