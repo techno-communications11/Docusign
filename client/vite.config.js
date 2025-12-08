53f3e6b22c9b7175760156f457b1ef4fff192dda
techno-communications11 <ptharun@techno-communications.com>
2025-12-08 22:03:46 +0530

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,34 +1,39 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-     hmr: {
-      host: "192.168.0.25",  // use the IP you are visiting from
+
+    // Allow the frontend domain you are using
+    allowedHosts: ["writeup.techno-communications.com"],
+
+    hmr: {
+      host: "192.168.0.25", // the IP you're accessing from (local dev)
       protocol: "ws",
-      port: 5173
+      port: 5173,
     },
+
     proxy: {
       "/login": {
-        target: "http://localhost:4503/auth",
+        target: "https://writeupapi.techno-communications.com/auth",
         changeOrigin: true,
       },
       "/users": {
-        target: "http://localhost:4503/auth",
+        target: "https://writeupapi.techno-communications.com/auth",
         changeOrigin: true,
       },
       "/register": {
-        target: "http://localhost:4503/auth",
+        target: "https://writeupapi.techno-communications.com/auth",
         changeOrigin: true,
       },
       "/reset-password": {
-        target: "http://localhost:4503/auth",
+        target: "https://writeupapi.techno-communications.com/auth",
         changeOrigin: true,
       },
       "/logout": {
-        target: "http://localhost:4503/auth",
+        target: "https://writeupapi.techno-communications.com/auth",
         changeOrigin: true,
       },
     },
